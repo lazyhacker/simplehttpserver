9044d87d38b3eb3d56e338d223a785ee8c82e859
LazyHackerAtWork <tsao@google.com>
2018-05-24 10:18:45 -0700

    add logging of requested URL

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-A http server similar to Python's SimpleHTTPServer with CORS support.
+A http server similar to Python's SimpleHTTPServer with CORS support. This is
+not meant to be a production server. It was written primarily to have a handy
+HTTP server available to use during developing.
+
+## Install
+
+`go install lazyhackergo.com/simplehttpserver`
+
+## Running
+
+In the root directory of your web app, run `simplehttpserver`.
--- simplehttpserver.go
+++ simplehttpserver.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, "hello, world")
+func handler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s", r.URL.String())
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	var port int
+	flag.IntVar(&port, "port", 8000, "default port for http server")
+	flag.Parse()
+	p := strconv.Itoa(port)
+
+	http.Handle("/", handler(http.FileServer(http.Dir("."))))
+	log.Printf("Starting HTTP on 127.0.0.1 port %s ...\n", p)
+	log.Fatal(http.ListenAndServe(":"+p, nil))
 }
